a15397e38a5712394356432e5f0fec39af11331c
VenuMannepalli <venu.mannepalli08@gmail.com>
2019-07-06 18:31:42 +0530

    readCSV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ReadAngularCSVFile';
+
+export class AppComponent  implements OnInit{
+ 
+  public searchText : string;
+  public items = [];
+  public tempItems=[];
+
+  constructor(private papa: Papa) {
+  }
+
+  ngOnInit(){
+  }
+
+  onChange(files: File[]){
+    var data: any[];
+    if(files[0]){
+      console.log(files[0]);
+      this.papa.parse(files[0], {
+        header: true,
+        skipEmptyLines: true,
+        complete: (result,file) => {
+          data=result.data;
+          this.items=data;
+          this.tempItems=data;
+        }
+      });
+    }
+  }
+
+  filter(searchTerm) {
+    console.log("search",searchTerm);
+    if(!searchTerm) {
+      this.items = this.tempItems;
+    } else {
+      this.items= this.tempItems.filter(x => 
+       // console.log("x",x);
+        x["Issue count"].replace(/\s/g, "").toLowerCase().includes(searchTerm.trim().toLowerCase())
+      );
+    }
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PapaParseModule } from 'ngx-papaparse';
+import { KeysPipe } from './keys.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,KeysPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    PapaParseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
